e6008428a13a8e1745b2ec8847787c280f01a7b8
Marius Ruh <mruh@ucsd.edu>
2020-05-18 16:40:58 -0700

    From Marius update: run.p

--- run.py
+++ run.py
@@ -31,6 +31,12 @@ import numpy as np
 from openmdao.api import Problem, Group, IndepVarComp, ExecComp, ScipyOptimizeDriver
 from UAM_team_optimization.components.cl_wing_comp import CLWingComp
 from UAM_team_optimization.components.cl_tail_comp import CLTailComp
+from UAM_team_optimization.components.cdi_wing_comp import CDiWingComp
+from UAM_team_optimization.components.cdi_tail_comp import CDiTailComp
+from UAM_team_optimization.components.geometry_comp import GeometryComp
+from UAM_team_optimization.components.propulsion_comp import wing_outer_prop_thrust_coeff, wing_inner_prop_thrust_coeff,tail_prop_thrust_coeff
+from UAM_team_optimization.components.axial_int_comp import AxialIntComp
+from UAM_team_optimization.components.percent_blown_comp import PercentBlownComp
 from UAM_team_optimization.components.geometry_comp import GeometryComp
 from UAM_team_optimization.components.grossweight_comp import GrossWeightComp
 from UAM_team_optimization.components.xcg_comp import XCGComp
@@ -41,26 +47,30 @@ model = Group()
 
 comp = IndepVarComp()
 
-# Wing
+#Adding Input variables which are the outputs of input_comp
+# Wing 
 comp.add_output('wing_alpha', val = 0.1)
 comp.add_output('wing_CLa', val = 2*np.pi)
 comp.add_output('wing_CL0', val = 0.2)
+comp.add_output('wing_CD0', val = 0.015)
+comp.add_output('wing_e', val = 0.85)
 comp.add_output('wing_AR', val = 8 )
 comp.add_output('wing_area', val = 25 )
-comp.add_output('wing_span', val=15.)
-
 # Tail
 comp.add_output('tail_alpha', val = 0)
 comp.add_output('tail_CLa', val = 2*np.pi)
 comp.add_output('tail_CL0', val = 0.2)
+comp.add_output('tail_CD0', val = 0.015)
+comp.add_output('tail_e', val = 0.85)
 comp.add_output('tail_AR', val = 8 )
 comp.add_output('tail_area', val = 4 )
-comp.add_output('tail_span', val=2.)
-
-# Propeller
+# Propeller 
 comp.add_output('wing_prop_inner_rad',val = 0.8)
 comp.add_output('wing_prop_outer_rad',val = 0.8)
 comp.add_output('tail_prop_rad',val = 0.8)
+comp.add_output('wing_inner_thrust_coeff', val = wing_inner_prop_thrust_coeff)
+comp.add_output('wing_outer_thrust_coeff', val = wing_outer_prop_thrust_coeff)
+comp.add_output('tail_thrust_coeff', val = tail_prop_thrust_coeff)
 
 comp.add_output('h', val=0.)
 comp.add_output('v_inf', val=58.)
@@ -88,8 +98,38 @@ comp.add_design_var('nmflts')
 
 model.add_subsystem('inputs_comp', comp, promotes = ['*'])
 
-# WEIGHTS/STABILITY COMPONENTS
 
+comp = GeometryComp()
+model.add_subsystem('geometry_comp', comp, promotes = ['*'])
+comp = ExecComp('wing_chord = wing_area / wing_span')
+model.add_subsystem('wing_chord_comp', comp, promotes = ['*'])
+comp = ExecComp('tail_chord = tail_area / tail_span')
+model.add_subsystem('tail_chord_comp', comp, promotes = ['*'])
+comp = AxialIntComp()
+model.add_subsystem('axial_int_comp', comp, promotes = ['*'])
+comp = PercentBlownComp()
+model.add_subsystem('percent_blown_comp', comp, promotes = ['*'])
+comp = CLWingComp()
+model.add_subsystem('cl_wing_comp', comp, promotes = ['*'])
+comp = CLTailComp()
+model.add_subsystem('cl_tail_comp', comp, promotes = ['*'])
+comp = CDiWingComp()
+model.add_subsystem('cdi_wing_comp', comp, promotes = ['*'])
+comp = CDiTailComp()
+model.add_subsystem('cdi_tail_comp', comp, promotes = ['*'])
+comp = ExecComp('wing_CD = wing_CD0 + wing_CDi')
+model.add_subsystem('wing_CD_comp', comp, promotes = ['*'])
+comp = ExecComp('tail_CD = tail_CD0 + tail_CDi')
+model.add_subsystem('tail_CD_comp', comp, promotes = ['*'])
+comp = ExecComp('wing_LD = wing_CL/wing_CD')
+model.add_subsystem('wing_ld_comp', comp, promotes = ['*'])
+comp = ExecComp('tail_LD = tail_CL/tail_CD')
+model.add_subsystem('tail_ld_comp', comp, promotes = ['*'])
+
+
+
+
+# WEIGHTS/STABILITY COMPONENTS
 comp = GrossWeightComp()
 model.add_subsystem('grossweight_comp', comp, promotes=['*'])
 
@@ -100,22 +140,29 @@ comp = XNPComp()
 model.add_subsystem('xnp_comp', comp, promotes=['*'])
 
 comp = ExecComp('SM = (XNP - XAC) / MAC')
-comp.add_constraint('SM', lower=0.06., upper=0.20.)
+comp.add_constraint('SM', lower=0.06, upper=0.20)
 model.add_subsystem('sm_comp', comp, promotes=['*'])
 
 comp = ExecComp('M = (wing_lift * XCG) - tail_lift * (tail_arm - XCG)')
 comp.add_constraint('M', equals=0.)
 model.add_subsystem('m_comp', comp, promotes=['*'])
 
+
+
+
+
+
+
 prob.model = model
-
-prob.driver = ScipyOptimizeDriver()
-prob.driver.options['optimizer'] = 'SLSQP'
-prob.driver.options['tol'] = 1e-15
-prob.driver.options['disp'] = True
-
 prob.setup()
 prob.run_model()
-prob.run_driver()
-
 prob.check_partials(compact_print=True)
+prob.model.list_outputs()
+# prob.driver = ScipyOptimizeDriver()
+# prob.driver.options['optimizer'] = 'SLSQP'
+# prob.driver.options['tol'] = 1e-15
+# prob.driver.options['disp'] = True
+
+
+# prob.run_driver()
+
