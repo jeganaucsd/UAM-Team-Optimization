8dc8364acad5ef549be2c924dcd3f2cd020ee085
jaegana <60194184+jaegana@users.noreply.github.com>
2020-05-18 17:59:44 -0700

    From Jake: 18MAY2020 1759

--- UAM_team_optimization/components/weightsandstability/test_emptyweight_comp.py
+++ UAM_team_optimization/components/weightsandstability/test_emptyweight_comp.py
@@ -1,6 +1,7 @@
 import unittest
 
-from emptyweight_comp import EmptyWeightComp
+# from emptyweight_comp import EmptyWeightComp
+from UAM_team_optimization.components.weightsandstability.emptyweight_comp import EmptyWeightComp
 from openmdao.api import Problem
 from openmdao.utils.assert_utils import assert_check_partials
 
