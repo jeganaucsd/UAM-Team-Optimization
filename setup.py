41ac5eabd349d30d94526b999fa29845b4ba39e8
Marius Ruh <mruh@ucsd.edu>
2020-05-18 20:07:17 -0700

    From Marius

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+
+setup(
+    name='UAM_team_optimzation',
+    version='1',
+    packages=[
+        'UAM_team_optimization',
+    ],
+    install_requires=[
+    ],
+)
