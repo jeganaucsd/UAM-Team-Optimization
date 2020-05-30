1c28976d15192ea74f991e6a566037f31b384fb2
Marius Ruh <mruh@ucsd.edu>
2020-05-30 16:36:05 -0700

    From MArius: Frist test after pushing red button

--- UAM_team_optimization/energy_group.py
+++ UAM_team_optimization/energy_group.py
@@ -9,3 +9,25 @@ class EnergyGroup(Group):
 
     def setup(self):
         shape = self.options['shape']
+        from openmdao.api import Group, IndepVarComp,ExecComp
+from lsdo_utils.api import PowerCombinationComp, LinearCombinationComp, LinearPowerCombinationComp
+
+
+class EnergyGroup(Group):
+    
+    def initialize(self):
+        self.options.declare('shape', types=tuple)
+
+    def setup(self):
+        shape = self.options['shape']
+
+        comp = PowerCombinationComp(
+            shape = shape,
+            out_name = 'total_avail_energy',
+            coeff = 3600.,
+            powers_dict=dict(
+                battery_mass = 1.,
+                batter_energy_density = 1.,
+            )
+        )
+        self.add_subsystem('total_avail_energy_comp', comp, promotes = ['*'])
--- UAM_team_optimization/motion_equations_group.py
+++ UAM_team_optimization/motion_equations_group.py
@@ -78,3 +78,42 @@ class MotionEquationsGroup(Group):
             constant = 0.,          
         )
         self.add_subsystem('rolling_moment_equilibrium_comp', comp, promotes = ['*'])
+
+        comp = LinearCombinationComp(
+            shape =shape,
+            out_name = 'wing_lift_moment_arm',
+            constant = 0.,
+            coeffs_dict = dict(
+                xcg = 1.,
+                nose_wing_c4 = -1.,
+            )
+        )
+        self.add_subsystem('wing_lift_moment_arm_comp', comp, promotes = ['*'])
+
+        comp = LinearCombinationComp(
+            shape =shape,
+            out_name = 'tail_lift_moment_arm',
+            constant = 0.,
+            coeffs_dict = dict(
+                nose_tail_c4= 1.,
+                xcg = -1.,
+            )
+        )
+        self.add_subsystem('tail_lift_moment_arm_comp', comp, promotes = ['*'])
+
+        comp = LinearPowerCombinationComp(
+            shape = shape,
+            out_name = 'pitching_moment_equilibrium',
+            terms_list=[
+                (1, dict(
+                    wing_lift_moment_arm = 1.,
+                    wing_lift = 1.,
+                )),
+                (-1, dict(
+                    tail_lift_moment_arm = 1.,
+                    tail_lift = 1.,
+                )),
+            ],
+            constant = 0.,          
+        )
+        self.add_subsystem('pitching_moment_equilibrium_comp', comp, promotes = ['*'])
